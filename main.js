0a9d356ba6097a79388ca8d8b6ca0c291704270d
Derian50 <43443693+Derian50@users.noreply.github.com>
2019-08-01 14:12:48 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ var button = document.getElementById('button')
 var container = document.getElementById('container')
 var messages = document.getElementById('messages')
 var form;
-console.log(input)
-console.log(button)
+var thought = true;
 button.onclick = function(){
 	dialog()
 }
@@ -14,25 +13,26 @@ input.onkeypress = function(e){
 	}
 }
 function dialog(){
-	console.log('SUCK')
-		console.log(input.value)
 		
 		var d = document.createElement('div')
 		d.className = 'text-left float-left col-7 alert alert-dark'
 		d.setAttribute('role' , 'alert')
-		if(input.value.length > 22){
-			input.value = input.value.substr(0,22)
-		}
+		
 		d.innerHTML = input.value
-		messages.appendChild(d)
+		messages.insertBefore(d, messages.firstChild)
 		input.value = ''
 
-		setTimeout(function(){
-			var d = document.createElement('div')
-			d.className = 'text-right float-right col-7 alert alert-dark'
-			d.setAttribute('role' , 'alert')
-			d.innerHTML = 'Понимаю'
-			messages.appendChild(d)
-		},Math.random()*2000)
+		if(thought){
+			setTimeout(function(){
+
+				thought = true;
+				var d = document.createElement('div')
+				d.className = 'text-right float-right col-7 alert alert-dark'
+				d.setAttribute('role' , 'alert')
+				d.innerHTML = 'Понимаю'			
+				messages.insertBefore(d, messages.firstChild)
+			},(Math.random()+0,1)*2000)
+		}
+		thought = false;
 
 }
